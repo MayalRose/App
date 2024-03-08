a690a94e4acdd2ff76a8dad3352a0ba3b831cf84
HirotoY107713 <hirotoyoki12345@gmail.com>
2024-03-09 04:35:32 +0800

    first commit

---
+++ src/components/trx-pools.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { StreamrClient } from 'streamr-client';
+
+export function TransactionPools() {
+    
+    return (
+        <></>
+    )
+}
